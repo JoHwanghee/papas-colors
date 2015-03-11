487b1fd08b5809d0d030e14d7d579f81cf83549e
johwanghee <johwanghee@gmail.com>
2015-03-11 18:25:47 +0900

    patch js

--- public/js/colors.js
+++ public/js/colors.js
@@ -1,25 +1,79 @@
-var start, end;
-var colors;
+var _gStart, _gEnd;
+var _gColors;
 
 $(function() {
-	console.log("Synchronous parse start");
-	$.get('assets/colors.csv', function(data) {
-		start = performance.now();
-		var results = Papa.parse(data, buildConfig());
-		//console.log("Synchronous parse results:", results);
-		colors = results.data;
-		displayColors(colors);
-	});
+	var cstring;
+	if(typeof(Storage) !== "undefined") {
+		cstring = localStorage.getItem("johwanghee.papas.colors");
+	}
 
-	$("#searchText").change(function() {
-		if($(this).val() == '') {
-			displayColors(colors);
-		} else {
-			var temp = [];
+	if(cstring != undefined && cstring.length > 0) {
+		parseData(cstring);
+	} else {
+		console.log("color parse start");
+		$.get(url+"assets/colors.csv", function(data) {
+			parseData(data);
 			
+			if(typeof(Storage) !== "undefined") {
+				localStorage.setItem("johwanghee.papas.colors",data);
+			}
+		});
+	}
+
+	$("#searchText").keyup(function() {
+		if($(this).val() == '') {
+			displayColors(_gColors);
+		} else {
+			var temps = [];
+			var key;
+			var keys = $(this).val().split(',');
+			var c;
+			for(key in keys) {
+				c = getColors(keys[key]);
+				if(c.length > 0) {
+					temps.push.apply(temps, c);
+				}
+			}
+			displayColors(temps);
 		}
 	});
 });
+
+function parseData(data) {
+	_global_start = performance.now();
+	var results = Papa.parse(data, buildConfig());
+	_gColors = results.data;
+	displayColors(_gColors);
+}
+
+function getColors(key) {
+	var temp = [];
+	var i;
+	var color;
+	if(key != undefined && key.length > 0) {
+		for(i in _gColors) {
+			color = _gColors[i];
+			switch(_gSearchType) {
+				case 'NO':
+					if(color[2].toUpperCase().indexOf(key.toUpperCase()) > -1) {
+						temp.push(color);
+					}
+					break;
+				case 'NM':
+					if(color[5].toUpperCase().indexOf(key.toUpperCase()) > -1) {
+						temp.push(color);
+					}
+					break;
+				case 'HX':
+					if(color[4].indexOf(key) > -1) {
+						temp.push(color);
+					}
+					break;
+			}
+		}
+	}
+	return temp;
+}
 
 function buildConfig() {
 	return {
@@ -46,11 +100,12 @@ function errorFn(error, file) {
 }
 
 function completeFn() {
-	end = performance.now();
+	_gEnd = performance.now();
 }
 
 var colorEliment = '<div class="color" style="background:#{color};">{no}<br/>{name}&nbsp;-&nbsp;0x{hex}<br/>{type}&nbsp;:&nbsp;{matt}</div>';
 function displayColors(colors) {
+	$("#colors").empty();
 	var i,len;
 	var el;
 	var color;
@@ -68,8 +123,8 @@ function displayColors(colors) {
 	}
 }
 
-var type;
-function changeCType(t) {
-	type = t;
-	$("#searchType").text(t);
+var _gSearchType = 'NO';
+function changeCType(type) {
+	_gSearchType = type;
+	$("#searchType").text(type);
 }
