b6d7b09e271700fb0bbd9e12f02f05a611edc70a
johwanghee <johwanghee@gmail.com>
2015-03-12 15:00:58 +0900

    add brand search

--- public/js/colors.js
+++ public/js/colors.js
@@ -1,5 +1,5 @@
 var _gStart, _gEnd;
-var _gColors;
+var _gColors = {};
 
 $(function() {
 	var cstring;
@@ -19,22 +19,7 @@ $(function() {
 	}
 
 	$("#searchText").keyup(function() {
-		if($(this).val() == '') {
-			_gTempColor = _gColors;
-			displayColors(_gColors);
-		} else {
-			var temps = [];
-			var key;
-			var keys = $(this).val().split(',');
-			var c;
-			for(key in keys) {
-				c = getColors(keys[key]);
-				if(c.length > 0) {
-					temps.push.apply(temps, c);
-				}
-			}
-			displayColors(temps);
-		}
+		search($(this).val());
 	});
 
 	$(window).scroll(function() {
@@ -44,21 +29,52 @@ $(function() {
 	});
 });
 
-function parseData(data) {
-	_global_start = performance.now();
-	var results = Papa.parse(data, buildConfig());
-	_gColors = results.data;
-	displayColors(_gColors);
+function search(searchKeys) {
+	if(searchKeys == '') {
+		displayColors(_gColors[_gBrand]);
+	} else {
+		var temps = [];
+		var i, key;
+		var keys = searchKeys.split(',');
+		var c;
+		for(i in keys) {
+			key = keys[i];
+			if(key.length > 1) {
+				c = getColors(_gColors[_gBrand], _gSearchType, key);
+				if(c.length > 0) {
+					temps.push.apply(temps, [[0,'▽&nbsp;&nbsp;&nbsp;" '+key+' " search result : '+c.length+'&nbsp;&nbsp;&nbsp;▽']]);
+					temps.push.apply(temps, c);
+				}
+			}
+		}
+		displayColors(temps);
+	}
 }
 
-function getColors(key) {
+function parseData(data) {
+	_gStart = performance.now();
+	var results = Papa.parse(data, buildConfig());
+	_gColors['all'] = results.data;
+	var i, temp, type;
+	for(i in results.data) {
+		temp = results.data[i];
+		type = temp[1];
+		if(_gColors[type] == undefined) {
+			_gColors[type] = [];
+		}
+		_gColors[type].push(temp);
+	}
+	displayColors(_gColors['all']);
+}
+
+function getColors(data, type, key) {
 	var temp = [];
 	var i;
 	var color;
 	if(key != undefined && key.length > 0) {
-		for(i in _gColors) {
-			color = _gColors[i];
-			switch(_gSearchType) {
+		for(i in data) {
+			color = data[i];
+			switch(type) {
 				case 'NO':
 					if(color[2].toUpperCase().indexOf(key.toUpperCase()) > -1) {
 						temp.push(color);
@@ -70,7 +86,7 @@ function getColors(key) {
 					}
 					break;
 				case 'HX':
-					if(color[4].indexOf(key) > -1) {
+					if(color[4].toUpperCase().indexOf(key.toUpperCase()) > -1) {
 						temp.push(color);
 					}
 					break;
@@ -111,38 +127,54 @@ function completeFn() {
 var _gTempColor;
 function displayColors(data) {
 	if(data != undefined) {
-		_gTempColor = data;
+		_gTempColor = data.slice(0);
 		$("#colors").empty();
 	}
 	if(_gTempColor == undefined || _gTempColor.length == 0) {
 		return false;
 	}
 	var colors = _gTempColor.slice(0);
-	var cel = '<div class="color" style="background:#{color};">{cnt}.&nbsp;{no}<br/>{name}&nbsp;-&nbsp;0x{hex}<br/>{type}&nbsp;:&nbsp;{matt}</div>';
+	var topCel = '<div class="color" style="background:#000;text-align:center;">{title}<br/></div>';
+	var cel = '<div class="color" style="background:#{color};">{no}<br/>{name}&nbsp;-&nbsp;0x{hex}<br/>{type}&nbsp;:&nbsp;{matt}</div>';
 	var i,len;
 	var el;
 	var color;
 	len = colors.length;
-	if(len > 50) {
-		len = 50;
-	}
+	if(len > 50) len = 50;
+
 	for(i=0;i<len;++i) {
 		color = colors[i];
-		el = cel;
-		el = el.replace("{cnt}",color[0]);
-		el = el.replace("{color}",color[4]);
-		el = el.replace("{no}",color[2]);
-		el = el.replace("{name}",color[5]);
-		el = el.replace("{hex}",color[4]);
-		el = el.replace("{type}",color[6]);
-		el = el.replace("{matt}",color[7]);
+		if(color[0] == 0) {
+			el = topCel;
+			el = el.replace("{title}",color[1]);
+		} else {
+			el = cel;
+			el = el.replace("{color}",color[4]);
+			el = el.replace("{no}",color[2]);
+			el = el.replace("{name}",color[5]);
+			el = el.replace("{hex}",color[4]);
+			el = el.replace("{type}",color[6]);
+			el = el.replace("{matt}",color[7]);
+		}
 		$("#colors").append(el);
 		_gTempColor.splice( 0, 1 );
 	}
 }
 
 var _gSearchType = 'NO';
-function changeCType(type) {
+var _gBrand = 'all';
+var _gSearchTypeLabel = {"NO":"Model No.","NM":"Color Name","HX":"Hex Code"}
+function changeSearchType(type) {
 	_gSearchType = type;
-	$("#searchType").text(type);
+	var txt = "Search Type({type})";
+	txt = txt.replace("{type}",_gSearchTypeLabel[type]);
+	$("#searchType").text(txt);
+}
+
+function changeBrand(brand) {
+	_gBrand = brand;
+	var txt = "Brand({brand})";
+	txt = txt.replace("{brand}",brand);
+	$("#brand").text(txt);
+	search($("#searchText").val());
 }
